0399fe8dd1c82133a6e4a2bd7df728bcbc2c44a2
glebl <gleb.lipai@yandex.ru>
2021-05-05 17:42:17 +0300

    9.1. String [#237407 #678]

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -4,10 +4,10 @@ public class Greeting {
     public static void main(String[] args) {
         String idea = "I like Java!";
         System.out.println(idea);
-        String ideaNext = idea + " But I am a newbie.";
+        idea += " But I am a newbie.";
         short year = 2021;
-        String ideaYear = ideaNext + " " + year;
-        System.out.println(ideaYear);
+        idea += " " + year;
+        System.out.println(idea);
 
     }
 
